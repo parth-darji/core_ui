237871ab9e964e9a19823a6029ff2160b30095e0
Parth Darji <parthdarji2961@gmail.com>
2026-07-05 13:36:55 +0530

    feat: remove soft_edge_blur export; bump to 1.2.0

--- lib/core_ui.dart
+++ lib/core_ui.dart
@@ -46,4 +46,3 @@ export 'src/widgets/ui_tooltip_info.dart';
 export 'src/widgets/ui_safe_bottom_spacing.dart';
 export 'src/widgets/ui_table.dart';
 export 'src/widgets/ui_divider.dart';
-export 'package:soft_edge_blur/soft_edge_blur.dart';
